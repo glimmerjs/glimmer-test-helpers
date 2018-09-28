4c7a439aaacf3aa166f567da56ae37a718dff8ae
Jonathan Jackson <jonathan.jackson1@me.com>
2018-09-28 16:54:36 -0400

    Allow testContainer compontn to set its context to the testContext
    This allows us to write tests that look like this:
    
    ```js
    test('Should do something', async function(assert) {
      this.foo = 'bar';
      await render(hbs`<p>{{this.foo}}</p>`);
    
      assert.dom('p').text('bar');
    });
    ```
    
    We override the ComponentManager's `getSelf` and provide a reference to
    the test's `this` context.
    
    Co-authored-by: Robert Jackson <me@rwjblue.com>

--- src/setup-rendering-test.ts
+++ src/setup-rendering-test.ts
@@ -1,11 +1,16 @@
-import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
-import { getApp } from './app';
+import Component, {
+  ComponentManager,
+  setPropertyDidChange,
+  RootReference
+} from "@glimmer/component";
+
+import { getApp } from "./app";
 
 type SerializedTemplateWithLazyBlock = any;
 
 export default function setupRenderingTest(hooks: NestedHooks): void {
   hooks.beforeEach(function beforeEach() {
-    this.app = new (getApp() as any);
+    this.app = new (getApp() as any)();
     this.render = render.bind(this);
     this.settled = settled.bind(this);
   });
@@ -15,9 +20,15 @@ export default function setupRenderingTest(hooks: NestedHooks): void {
   });
 }
 
-function render(precompiledTemplate: SerializedTemplateWithLazyBlock): Promise<void> {
-  let app = this.app;
-  let containerElement = document.createElement('div');
+function render(
+  precompiledTemplate: SerializedTemplateWithLazyBlock
+): Promise<void> {
+  precompiledTemplate.meta.managerId = "test-container";
+
+  let testContext = this;
+  let app = testContext.app;
+
+  let containerElement = document.createElement("div");
 
   setPropertyDidChange(() => {
     app.scheduleRerender();
@@ -25,26 +36,43 @@ function render(precompiledTemplate: SerializedTemplateWithLazyBlock): Promise<v
 
   app.registerInitializer({
     initialize(registry: any) {
-      // TODO: temporary hack until we consolidate registries
-      registry._resolver.registry._entries[`template:/${app.rootName}/components/test-container`] = precompiledTemplate;
-      registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
+      registry._resolver.registry._entries[
+        `template:/${app.rootName}/components/test-container`
+      ] = precompiledTemplate;
+
+      registry._resolver.registry._entries[
+        `component:/${app.rootName}/components/test-container`
+      ] = Component;
+
+      class CustomComponentManager extends ComponentManager {
+        static create(options: any) {
+          return new this(options);
+        }
+
+        getSelf() {
+          return new RootReference(testContext);
+        }
+      }
+
+      registry.register(
+        `component-manager:/${app.rootName}/component-managers/test-container`,
+        CustomComponentManager
+      );
     }
   });
 
-  app.renderComponent('test-container', containerElement);
-  return Promise.resolve(app.boot())
-    .then(() => {
-      this.containerElement = containerElement;
-      this.app = app;
-    });
+  app.renderComponent("test-container", containerElement);
+  return Promise.resolve(app.boot()).then(() => {
+    testContext.containerElement = containerElement;
+  });
 }
 
 async function settled(): Promise<void> {
   return new Promise<void>(resolve => {
     let watcher = setInterval(() => {
-      if (this.app['_rendering']) return;
+      if (this.app["_rendering"]) return;
       clearInterval(watcher);
       resolve();
     }, 10);
   });
-};
+}
