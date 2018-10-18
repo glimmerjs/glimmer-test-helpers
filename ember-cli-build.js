ba8ff4ab6662a0b7182c3e5b5a0095d243be491c
Jonathan Jackson <jonathan.jackson1@me.com>
2018-10-18 14:20:00 -0400

    Allow src to be a watched tree for rebuilds in dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,21 +1,41 @@
-const { GlimmerApp } = require('@glimmer/application-pipeline');
+const {
+  GlimmerApp
+} = require('@glimmer/application-pipeline');
 
 const build = require('@glimmer/build');
 const MergeTrees = require('broccoli-merge-trees');
 const Funnel = require('broccoli-funnel');
+const BroccoliDebug = require('broccoli-debug');
+const FileCreator = require('broccoli-file-creator');
+const fs = require('fs');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
+  let debugTree = BroccoliDebug.buildDebugCallback(`glimmer-test-helpers`);
+
   let external = [
     '@glimmer/component',
   ];
 
-  let libTree = build({ buildType: 'production', external, tsconfigPath: 'glimmer-build-tsconfig.json'});
+  let libTree = build({
+    buildType: 'production',
+    external,
+    tsconfigPath: 'glimmer-build-tsconfig.json'
+  });
+
+  let nodeModules = new MergeTrees([
+    'node_modules',
+    new Funnel(libTree, { destDir: '@glimmer/test-helpers/dist' }),
+    new FileCreator('@glimmer/test-helpers/package.json', fs.readFileSync('package.json', 'utf-8')),
+  ]);
+
+  nodeModules = debugTree(nodeModules, 'dummy-app-node-modules');
 
   let app = new GlimmerApp(defaults, {
     trees: {
       src: 'tests/dummy/src',
       public: 'tests/dummy/public',
-      styles: 'tests/dummy/src/ui/styles'
+      styles: 'tests/dummy/src/ui/styles',
+      nodeModules
     }
   });
 
@@ -23,12 +43,15 @@ module.exports = function(defaults) {
     exclude: ['dummy']
   });
 
-  let appTree = new MergeTrees([app.toTree(), testTree], { overwrite: true }); // TODO: @rondale-sc - fix me
+  let appTree = debugTree(app.toTree(), 'appTree');
+
+  appTree = new MergeTrees([appTree, testTree], {
+    overwrite: true
+  }); // TODO: @rondale-sc - fix me
 
   appTree = new Funnel(appTree, {
     destDir: 'tests'
   });
-
 
   return new MergeTrees([appTree, libTree]);
 };
